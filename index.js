03348bc9ce12fa1cefbbf3968c39d605682059de
Sahil Shetty <70585867+ACENSONIX@users.noreply.github.com>
2022-08-19 02:32:26 +0530

    added home page

--- index.js
+++ index.js
@@ -1,68 +1,74 @@
-var p = document.getElementsByTagName('p');
-var choice = document.getElementsByClassName('choice');
+var p = document.getElementsByTagName("p");
+var choice = document.getElementsByClassName("choice");
 
 var dragItem = null;
 
 console.log("hello");
 
-for(var i of p){
-    i.addEventListener('dragstart', dragStart);
-    i.addEventListener('dragend', dragEnd);
+for (var i of p) {
+  i.addEventListener("dragstart", dragStart);
+  i.addEventListener("dragend", dragEnd);
 }
 
-function dragStart(){
-    dragItem = this;
-    setTimeout(()=>this.style.display ="none", 0);
+function dragStart() {
+  dragItem = this;
+  setTimeout(() => (this.style.display = "none"), 0);
 }
 
-function dragEnd(){
-    setTimeout(()=> this.style.display = "block", 0);
-    dragItem = null;
+function dragEnd() {
+  setTimeout(() => (this.style.display = "block"), 0);
+  dragItem = null;
 }
 
-for(j of choice){
-    j.addEventListener('dragover', dragOver);
-    j.addEventListener('dragenter', dragEnter);
-    j.addEventListener('dragleave', dragLeave);
-    j.addEventListener('drop', Drop);
+for (j of choice) {
+  j.addEventListener("dragover", dragOver);
+  j.addEventListener("dragenter", dragEnter);
+  j.addEventListener("dragleave", dragLeave);
+  j.addEventListener("drop", Drop);
 }
 
-function Drop(){
-    this.append(dragItem);
+function Drop() {
+  this.append(dragItem);
 }
 
-function dragOver(e){
-    e.preventDefault();
-    this.style.border = "2px dotted red";
+function dragOver(e) {
+  e.preventDefault();
+  this.style.border = "2px dotted red";
 }
 
-function dragEnter(e){
-    e.preventDefault();
+function dragEnter(e) {
+  e.preventDefault();
 }
 
-function dragLeave(){
-    this.style.border = "none";
+function dragLeave() {
+  this.style.border = "none";
 }
 
-function check(){
-    let choice = document.getElementsByClassName('choice');
-    var reactant1 = choice[0].children;
-    var reactant2 = choice[1].children;
-    console.log(choice);
-    try {
-        if (reactant1[0].className == "sodium" && reactant1[1].className == "chlorine" || reactant1[0].className == "chlorine" && reactant1[1].className == "sodium"){
-            if (reactant2[0].className == "oxygen" && reactant2[1].className == "hydrogen" && reactant2[2].className == "hydrogen" || reactant2[0].className == "hydrogen" && reactant2[1].className == "oxygen" && reactant2[2].className == "hydrogen" || reactant2[0].className == "hydrogen" && reactant2[1].className == "hydrogen" &&reactant2[2].className == "oxygen"){
-                alert("Correct");
-            }
-            else{
-                alert("Incorrect. Please try again.");
-            }
-        }
-        else{
-            alert("Incorrect please try again.");
-        }
-    } catch (error) {
-        alert("Incorrect please try again.");
+function check() {
+  let choice = document.getElementsByClassName("choice");
+  var reactant1 = choice[0].children;
+  var reactant2 = choice[1].children;
+  console.log(choice);
+  try {
+    if (
+      (reactant1[0].className == "sodium" &&
+        reactant1[1].className == "chlorine") ||
+      (reactant1[0].className == "chlorine" &&
+        reactant1[1].className == "sodium")
+    ) {
+      if (
+        reactant2[1].className == "oxygen" &&
+        reactant2[0].className == "hydrogen" &&
+        reactant2[2].className == "hydrogen"
+      ) {
+        alert("Correct");
+      } else {
+        alert("Incorrect. Please try again.");
+      }
+    } else {
+      alert("Incorrect please try again.");
     }
-
+  } catch (error) {
+    alert("Incorrect please try again.");
+  }
 }

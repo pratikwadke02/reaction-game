c9a6a1806e7a36bda5d0daa190b1ab60b4004a03
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2023-04-20 12:22:48 +0530

    refactored files

--- CDAC-Node/routes/webRoutes/webroute.js
+++ CDAC-Node/routes/webRoutes/webroute.js
@@ -1,41 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-
-router.get('/home-page',(req,res)=>{
-    res.render('Home');
+router.get("/", (req, res) => {
+  res.render("home");
 });
 
-router.get('/neutralisation-reaction/:id',(req,res)=>{
-    res.render('neutralisation');
+router.get("/home", (req, res) => {
+  res.render("home");
 });
 
-router.get('/reaction-page/:id/:type',(req,res)=>{
-    res.render('displacement');
+router.get("/reaction-page/:id/:type", (req, res) => {
+  res.render("reaction");
 });
 
-router.get('/decomposition-reaction/:id',(req,res)=>{
-    res.render('decomposition');
+router.get("/reaction-list/:type", (req, res) => {
+  res.render("reactionList");
 });
 
-router.get('/combustion-reaction/:id',(req,res)=>{
-    res.render('combustion');
-});
-
-router.get('/neutralisation-list', (req, res)=> {
-    res.render('neutralisationList');
-});
-
-router.get('/combustion-list', (req, res)=> {
-    res.render('combustionList');
-});
-
-router.get('/reaction-list/:type', (req, res)=> {
-    res.render('displacementList');
-});
-
-router.get('/decomposition-list', (req, res)=> {
-    res.render('decompositionList');
-});
-
-module.exports = router
+module.exports = router;
